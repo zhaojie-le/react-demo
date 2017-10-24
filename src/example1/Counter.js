a237098be476847ed3b149b77b63c9e4d61baddc
zhaojie <zhaojie@daojia.com>
2017-10-24 19:32:08 +0800

    父子组件通信功能

--- src/example1/Counter.js
+++ src/example1/Counter.js
@@ -3,18 +3,40 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 class Counter extends Component {
   constructor (props) {
-    super(props)
-    this.onClickIncrementButton = this.onClickIncrementButton.bind(this)
-    this.onClickDecrementButton = this.onClickDecrementButton.bind(this)
+    super(props);
+    // 定义事件需要在构造函数中绑定this
+    this.onClickIncrementButton = this.onClickIncrementButton.bind(this);
+    this.onClickDecrementButton = this.onClickDecrementButton.bind(this);
+    // 定义组件内变量
     this.state = {
       count: props.initValue || 0
     }
   }
 
   onClickIncrementButton () {
-    this.setState({count: this.state.count + 1})
+    // this.setState({count: this.state.count + 1})
+    this.updataCount(true)
   }
 
+  updataCount (isIncrement) {
+    const oldValue = this.state.count
+    const newValue = isIncrement ? oldValue + 1 : oldValue - 1
+    this.setState({count: newValue})
+    // 向父组件通信函数，传递参数
+    this.props.onUpdate(newValue, oldValue)
+  }
+
+  onClickDecrementButton () {
+    // if (this.state.count > 0){
+    //   this.setState({count: this.state.count - 1})
+    // }
+    this.updataCount(false)
+  }
+
+
+  componentWillReceiveProps (nextProps) {
+    console.log('componentWillReceiveProps'+ this.props.caption)
+  }
   render () {
     const {caption} = this.props
     const buttonStyle = {
@@ -32,6 +54,11 @@ class Counter extends Component {
 }
 Counter.propTypes = {
   caption: PropTypes.string.isRequired,
-  initValue: PropTypes.number
+  initValue: PropTypes.number || 0
+}
+Counter.defaultProps = {
+  initValue: 0,
+  // 父子组件通信函数
+  onUpdata: f => f   // 默认是一个什么都不做的函数
 }
 export default Counter
